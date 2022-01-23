da260343c2fcba41d245cfe39acdff49d528a20a
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2022-01-23 23:47:15 +0100

    Completed module

---
+++ ssg/parsers.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import List
+import shutil
+
+class Parser:
+    extensions :List[str] =[]
+
+    def valid_extension(self,extension):
+        if extension in self.extensions:
+            return True
+        return False
+
+    def parse(path:Path,source:Path,dest:Path):
+        raise NotImplementedError()
+    
+    def read(path):
+        with open(path) as file :
+            return file.read()
+    def write(path,dest,content,ext=".html"):
+        full_path=dest / path.with_suffix() + ext
+        with open(full_path) as file:
+            file.write(content)
+    def copy(path,source,dest):
+        shutil.copy2(path,dest / path.relative_to(source))
+
+class ResourceParser (Parser):
+    extensions=[".jpg",".png",".gif",".css",".html"]
+
+    def parse(path:Path,source:Path,dest:Path):
+        super().copy(path,source,dest)
+        
+
+
+
+
+

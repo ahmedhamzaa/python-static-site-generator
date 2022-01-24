40ca0293ce88c489a4f539ca83b77e657b1e9330
ahmed hamza jandoubi <ahmedhamza0601@gmail.com>
2022-01-24 12:34:18 +0100

    Completed module

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,4 +1,9 @@
+from distutils import extension
 import shutil
+import sys
+from docutils.core import publish_parts
+from markdown import Markdown, markdown
+from ssg.content import Content
 
 from typing import List
 from pathlib import Path
@@ -31,3 +36,22 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content=Content.load(self.read(path))
+        html=Markdown(content.body)
+        self.write(path,dest,html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+
+class ReStructuredParser(Parser):
+    extensions=[".rst"]
+    def parse(self, path, source, dest):
+        content=Content.load(self.read(path))
+        html=publish_parts(content.body,writer_name="html5")
+        self.write(path,dest,html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+
+
